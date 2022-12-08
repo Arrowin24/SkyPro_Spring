4184f4d30d966949d4443ec1f13b8e5f86ba2ca5
Arrowin24 <igor_rudenko.ru@mail.ru>
2022-12-08 18:27:18 +0700

    Commit

---
+++ src/main/java/com/example/skypro_spring/SkyProSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.skypro_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skypro_spring/SkyProSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skypro_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
